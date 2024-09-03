506eec6f30ff6916d2bd345c090545de030a30d5
Deniz Dumbak <denizdmbk19@gmail.com>
2024-09-03 16:07:12 +0300

    todosReducer and usersReducers is stored

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todosReducer from "./ToDoReducer";
+import usersReducer from "./UserReducer";
 
 const store = configureStore({
     reducer: {
-        todos: todosReducer
+        todos: todosReducer,
+        users: usersReducer,
     }
 })
