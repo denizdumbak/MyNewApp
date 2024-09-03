3212a3687f7ac52c7f2f124a4a54729d139e842a
Deniz Dumbak <denizdmbk19@gmail.com>
2024-09-03 16:17:46 +0300

    App.js wrapped with 'Provider'

--- App.js
+++ App.js
@@ -1,22 +1,27 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import  LoginScreen  from './src/components/LoginScreen';
-import  SignupScreen  from './src/components/SignupScreen';
-import  ToDoScreen  from './src/components/ToDoScreen';
-import  UserScreen  from './src/components/UserScreen';
+import LoginScreen from './src/components/LoginScreen';
+import SignupScreen from './src/components/SignupScreen';
+import ToDoScreen from './src/components/ToDoScreen';
+import UserScreen from './src/components/UserScreen';
+import { Provider } from 'react-redux';
+import store from './src/redux/store';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name='login' component={LoginScreen} />
-        <Stack.Screen name='signup' component={SignupScreen} />
-        <Stack.Screen name='todo' component={ToDoScreen} />
-        <Stack.Screen name='user' component={UserScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name='login' component={LoginScreen} />
+          <Stack.Screen name='signup' component={SignupScreen} />
+          <Stack.Screen name='todo' component={ToDoScreen} />
+          <Stack.Screen name='user' component={UserScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
+
   );
 }
 
