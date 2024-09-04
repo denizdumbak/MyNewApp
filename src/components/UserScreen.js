b862e238af8943dda9e4ff21fa14ab35f98f621b
Deniz Dumbak <denizdmbk19@gmail.com>
2024-09-04 09:23:44 +0300

    UserScreen is designed

--- src/components/UserScreen.js
+++ src/components/UserScreen.js
@@ -1,14 +1,111 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react';
+import { View, Text, Button, FlatList, TouchableOpacity, StyleSheet } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
+import { deleteUser, fetchUsers } from '../redux/UserReducer';
 
-const UserScreen = () => {
+export default function UserList({ navigation }) {
+  const dispatch = useDispatch();
+  const { data, status, error } = useSelector(state => state.users);
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [dispatch])
+
+  if (status === "loading") {
+    return <Text>Loading...</Text>
+  }
+  if (status === "failed") {
+    return <Text>Error: {error}</Text>
+  }
+
   return (
-    <View>
-      <Text>UserScreen</Text>
+    <View style={styles.container}>
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            style={styles.card}
+            onPress={() => navigation.navigate('UserProfile', { user: item })}
+          >
+            <Text style={styles.userName}>{item.name}</Text>
+            <View style={styles.buttonContainer}>
+              <TouchableOpacity
+                style={styles.button}
+                onPress={() => navigation.navigate('UpdateUser', { user: item })}
+              >
+                <Text style={styles.buttonText}>Update</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                style={[styles.button, styles.deleteButton]}
+                onPress={() => dispatch(deleteUser(item.id))}
+              >
+                <Text style={styles.buttonText}>Delete</Text>
+              </TouchableOpacity>
+            </View>
+          </TouchableOpacity>
+        )}
+      />
+
+      <TouchableOpacity
+        style={styles.addButton}
+        title="Add User"
+        onPress={() => navigation.navigate('AddUser')}
+      >
+        <Text style={styles.buttonText}>Add User</Text>
+      </TouchableOpacity>
     </View>
-  )
+  );
 }
 
-export default UserScreen
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+  card: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: '#f9f9f9',
+    padding: 16,
+    marginBottom: 8,
+    borderRadius: 8,
+    shadowColor: '#000',
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+    elevation: 3,
+  },
+  userName: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  button: {
+    backgroundColor: '#81C784',
+    padding: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+    margin: 5,
 
-const styles = StyleSheet.create({})
+  },
+  deleteButton: {
+    backgroundColor: '#F44336',
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  addButton: {
+    backgroundColor: '#0091EA',
+    padding: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+    margin: 5,
+  }
+});
