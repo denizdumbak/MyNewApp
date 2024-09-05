88d83826c8e1505d4e97cfda2cb2f4c6070cf809
Deniz Dumbak <denizdmbk19@gmail.com>
2024-09-05 10:56:17 +0300

    ToDoScreen is designed

--- src/components/ToDoScreen.js
+++ src/components/ToDoScreen.js
@@ -1,14 +1,97 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList, StyleSheet, TouchableOpacity } from 'react-native';
+import { useDispatch } from 'react-redux';
+import { deleteTodo } from '../redux/ToDoReducer';
 
-const ToDoScreen = () => {
+export default function ToDoScreen({ route }) {
+  const { todos } = route.params;
+  const dispatch = useDispatch();
+
+  const handleDelete = (id) => {
+    console.log('Attempting to delete todo with id:', id);
+    dispatch(deleteTodo(id));
+  };
+
   return (
-    <View>
-      <Text>ToDoScreen</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>To-Do List</Text>
+      <FlatList
+        data={todos}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.todoItem}>
+            <View style={styles.todoTextContainer}>
+              <Text style={styles.todoTitle}>{item.title}</Text>
+              <Text style={item.completed ? styles.completed : styles.notCompleted}>
+                {item.completed ? "Completed" : "Not Completed"}
+              </Text>
+            </View>
+            <TouchableOpacity
+              style={styles.deleteButton}
+              onPress={() =>handleDelete(item.id)}
+            >
+              <Text style={styles.deleteText}>Delete</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+      />
     </View>
-  )
+  );
 }
 
-export default ToDoScreen
-
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#f0f4f8',
+  },
+  title: {
+    fontSize: 26,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    color: '#2c3e50',
+  },
+  todoItem: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: '#ffffff',
+    padding: 16,
+    borderRadius: 12,
+    marginBottom: 12,
+    shadowColor: '#000',
+    shadowOpacity: 0.1,
+    shadowOffset: { width: 0, height: 4 },
+    shadowRadius: 6,
+    elevation: 3,
+  },
+  todoTextContainer: {
+    flex: 1,
+  },
+  todoTitle: {
+    fontSize: 18,
+    fontWeight: '600',
+    color: '#34495e',
+  },
+  completed: {
+    marginTop: 4,
+    fontSize: 14,
+    color: '#27ae60',
+  },
+  notCompleted: {
+    marginTop: 4,
+    fontSize: 14,
+    color: '#e74c3c',
+  },
+  deleteButton: {
+    alignSelf: 'center',
+    backgroundColor: '#F44336',
+    height: 35,
+    width: 70,
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  deleteText: {
+    fontWeight: 'bold',
+    color: 'white'
+  },
+});
